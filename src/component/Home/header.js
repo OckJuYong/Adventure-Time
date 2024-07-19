6845cf2afc1eb5d36f9b8b1abd86b7b1d744b70e
OckJuYong <20201776@edu.hanbat.ac.kr>
2024-07-19 09:16:50 +0900

    header plus_(1)

---
+++ src/component/Home/header.js
@@ -0,0 +1,20 @@
+
+import React, { useState } from 'react';
+
+
+import headersty from './header.module.css';
+
+function Footer() {  // 대문자로 변경
+    const [address, setAddress] = useState("대전");
+  return (
+    <div className={headersty.header_container}>
+        <div className={headersty.healing}>휴식이 필요한 오늘 같은 날</div>
+        <div className={headersty.read_container}>
+            <div className={headersty.address}>현위치 : {address}</div>
+            <div className={headersty.read}>여행 준비 중</div>
+        </div>
+    </div>
+  );
+}
+
+export default Footer;  // 대문자로 변경
--- src/component/Home/home.js
+++ src/component/Home/home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Footer from '../footer/footer';
 import Main from '../main/main';
+import Header from '../Home/header';
 
 
 function Home() {  
   return (
     <div className=''>
+      <Header />
       <Main />
       <Footer /> 
     </div>
