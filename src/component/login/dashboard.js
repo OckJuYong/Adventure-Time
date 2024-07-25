cefcbcf02e142986abbd4d862afe5103393740c3
OckJuYong <20201776@edu.hanbat.ac.kr>
2024-07-25 23:23:37 +0900

    qq

--- src/component/login/dashboard.js
+++ src/component/login/dashboard.js
@@ -15,6 +15,7 @@ function Dashboard() {
           withCredentials: true // 쿠키를 포함하기 위해 필요합니다
         });
         setUserInfo(response.data);
+        console.log(response.data);
       } catch (error) {
         console.error("사용자 정보 가져오기 실패:", error);
         setError("사용자 정보를 가져오는데 실패했습니다.");
@@ -41,8 +42,7 @@ function Dashboard() {
         <div>
           <h2>사용자 정보</h2>
           <p>이름: {userInfo.name}</p>
-          <p>이메일: {userInfo.email}</p>
-          {/* 추가적인 사용자 정보를 여기에 표시 */}
+          <p>이메일: {userInfo.member.email}</p>
         </div>
       ) : (
         <p>사용자 정보를 불러오지 못했습니다.</p>
