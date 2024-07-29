6647f2a37854355855abbf51a4f0b08d0751f65b
OckJuYong <20201776@edu.hanbat.ac.kr>
2024-07-29 22:44:35 +0900

    are

--- src/component/mate_prop1/mateprop1.js
+++ src/component/mate_prop1/mateprop1.js
@@ -65,10 +65,10 @@ function Mateprop1() {
         const selectedSlide = slides[index1];
         try {
             const response = await axios.post('https://port-0-travelproject-9zxht12blqj9n2fu.sel4.cloudtype.app/friend/additional', {
-                friendTravelUserId: selectedSlide.travelUserId
+                friendTravelUserId: slides[selectedSlide.travelUserId].travelUserId
             });
             console.log('Propose response:', response.data);
-            console.log(selectedSlide.travelUserId);
+            console.log(slides[selectedSlide.travelUserId].travelUserId);
 
         } catch (error) {
             console.error('Error proposing:', error);
